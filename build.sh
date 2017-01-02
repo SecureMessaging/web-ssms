86ea761aebb64da47193426a533597aeeb4d7d4c
Evan <evan.hurd@renweb.com>
2017-01-02 09:45:55 -0600

    Add Build File

--- build.sh
+++ build.sh
@@ -1,16 +1,12 @@
 #! /bin/bash
 
-dir=`mktemp -d -t build`
 package_name = "web_ssms"
 package_version = "0.0.1"
 target = "test"
 tar_file = $package_name + "." + $package_version + "-" + $target + ".tar"
 
-cd $dir
-
-git clone https://github.com/SecureMessaging/apps-ssms.git $dir
 npm install
 ng build
-cp $dir/package.json $dir/dist/package.json
-tar -czf $dir/$tar_file --directory="$dir/dist"
-npm publish $dir/$tar_file --access public
+cp $BUILD_DIR/package.json $BUILD_DIR/dist/package.json
+tar -czf $BUILD_DIR/$tar_file --directory="$BUILD_DIR/dist"
+npm publish $BUILD_DIR/$tar_file --access public
