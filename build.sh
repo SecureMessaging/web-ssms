7b6bb0f49928160b2d4cb2186f0f31b732b4253b
Evan <evan.hurd@renweb.com>
2017-01-02 12:44:38 -0600

    Changes

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ tar_file="$package_name.$version.tar"
 npm install
 tsc
 #cp $BUILD_DIR/package.json $BUILD_DIR/dist/package.json
-sed "3s/.*/\"version\": \"$version\"/" $BUILD_DIR/package.json > $BUILD_DIR/dist/package.json
-sed -i "2s/.*/\"name\": \"$package_name\"/" $BUILD_DIR/dist/package.json
+sed "3s/.*/  \"version\": \"$version\",/" $BUILD_DIR/package.json > $BUILD_DIR/dist/package.json
+sed -i "2s/.*/  \"name\": \"$package_name\",/" $BUILD_DIR/dist/package.json
 echo "Creating TAR file: $BUILD_DIR/$tar_file from $BUILD_DIR/dist"
 tar -czf $BUILD_DIR/$tar_file --directory="$BUILD_DIR/dist" .
 npm publish $BUILD_DIR/$tar_file --access public
@@ -19,3 +19,4 @@ npm publish $BUILD_DIR/$tar_file --access public
 #/tmp/tmp-5638kR934fFhYOF2/
 
 tar -czf /tmp/tmp-5638kR934fFhYOF2//$tar_file --directory="/tmp/tmp-5638kR934fFhYOF2//dist" *
+
