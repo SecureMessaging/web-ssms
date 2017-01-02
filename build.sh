54fde2a3e6565be4aa45e38253208e43b91c5102
Evan <evan.hurd@renweb.com>
2017-01-02 10:52:31 -0600

    fix build

--- build.sh
+++ build.sh
@@ -8,5 +8,10 @@ tar_file="$package_name.$package_version.$target.tar"
 npm install
 tsc
 cp $BUILD_DIR/package.json $BUILD_DIR/dist/package.json
-tar -czf $BUILD_DIR/$tar_file --directory="$BUILD_DIR/dist"
+tar -czf $BUILD_DIR/$tar_file --directory="$BUILD_DIR/dist" .
 npm publish $BUILD_DIR/$tar_file --access public
+
+#cp $BUILD_DIR/package.json $BUILD_DIR/dist/package.json
+#/tmp/tmp-5638kR934fFhYOF2/
+
+tar -czf /tmp/tmp-5638kR934fFhYOF2//$tar_file --directory="/tmp/tmp-5638kR934fFhYOF2//dist" *
