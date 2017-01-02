a9eb0dde92645fa4545358e1cc7a1e7bfc1e7062
Evan <evan.hurd@renweb.com>
2017-01-02 11:24:07 -0600

    fix build

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
 #! /bin/bash
 
-package_name="web_ssms"
+package_name="web-ssms-$target"
 package_version="0.0.1"
-target="test"
 tar_file="$package_name.$package_version.$target.tar"
+latest_version=`npm show $package_name version`
+
+if [ $latest_version == "undefined" ]; then
+    latest_version="0.0.1"
+fi 
+
+version="$latest_version"
 
 npm install
 tsc
-cp $BUILD_DIR/package.json $BUILD_DIR/dist/package.json
+#cp $BUILD_DIR/package.json $BUILD_DIR/dist/package.json
+sed "3s/.*/"version": "$version"/" $BUILD_DIR/package.json > $BUILD_DIR/dist/package.json
 echo "Creating TAR file: $BUILD_DIR/$tar_file from $BUILD_DIR/dist"
 tar -czf $BUILD_DIR/$tar_file --directory="$BUILD_DIR/dist" .
 npm publish $BUILD_DIR/$tar_file --access public
