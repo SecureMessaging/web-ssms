0c77fc6eb0589fd8237e1eee7b41e942411c2c80
Evan <evan.hurd@renweb.com>
2017-01-02 11:47:40 -0600

    fixes

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ if [ "$latest_version" == 'undefined' ]; then
     latest_version="0.0.0"
 fi 
 
-version=`node -e "console.log(parseInt('$latest_version'.split('.').pop()) + 1)"`
+version=`node -e "var v = '$latest_version'.split('.'); console.log(v[0] + '.' + v[1] + '.' + parseInt(v[2]) + 1)"`
 tar_file="$package_name.$version.tar"
 
 npm install
 tsc
 #cp $BUILD_DIR/package.json $BUILD_DIR/dist/package.json
-sed '3s/.*/"version": "$version"/' $BUILD_DIR/package.json > $BUILD_DIR/dist/package.json
-sed '2s/.*/"version": "$version"/' $BUILD_DIR/dist/package.json
+sed "3s/.*/\"version\": \"$version\"/" $BUILD_DIR/package.json > $BUILD_DIR/dist/package.json
+sed -i "2s/.*/\"name\": \"$package_name\"/" $BUILD_DIR/dist/package.json
 echo "Creating TAR file: $BUILD_DIR/$tar_file from $BUILD_DIR/dist"
 tar -czf $BUILD_DIR/$tar_file --directory="$BUILD_DIR/dist" .
 npm publish $BUILD_DIR/$tar_file --access public
