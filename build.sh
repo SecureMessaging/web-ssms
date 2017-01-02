73739f40f63ad1adf8d8059e23c5c97b9deaa16b
Evan <evan.hurd@renweb.com>
2017-01-02 13:05:43 -0600

    Chemists don't die, they just stop reacting!

--- build.sh
+++ build.sh
@@ -6,9 +6,10 @@ latest_version=`npm show $package_name version`
 version=`node -e "var v = '$latest_version'.split('.'); console.log((parseInt(v[0]) || 0) + '.' + (parseInt(v[1]) || 0) + '.' + ((parseInt(v[2]) || 0) + 1))"`
 tar_file="$package_name.$version.tar"
 
+wcho "Bulding $package_name@$version"
+
 npm install
 tsc
-#cp $BUILD_DIR/package.json $BUILD_DIR/dist/package.json
 sed "3s/.*/  \"version\": \"$version\",/" $BUILD_DIR/package.json > $BUILD_DIR/dist/package.json
 sed -i "2s/.*/  \"name\": \"$package_name\",/" $BUILD_DIR/dist/package.json
 echo "Creating TAR file: $BUILD_DIR/$tar_file from $BUILD_DIR/dist"
