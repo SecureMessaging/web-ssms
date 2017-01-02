6e747dd046bfb0bb32039e868f6379c5fe171462
Evan <evan.hurd@renweb.com>
2017-01-02 12:14:15 -0600

    fixes

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ package_name="web-ssms-$BUILD_TARGET"
 latest_version=`npm show $package_name version`
 size=${#latest_version}
 
-if [ "$latest_version" == 'undefined' && $size gt 8 ]; then
+if [ "$latest_version" == 'undefined' && "$size" gt '8' ]; then
     latest_version="0.0.0"
 fi 
 
